d5d95408c07b267290b870d1e2bbd60c370c3322
Faizan-Lakhani <136820578+Faizan-Lakhani@users.noreply.github.com>
2023-07-09 11:24:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+alert("Welcome dear")
+var first_number = 3
+var second_number = 5
+document.write("Sum of two variables : ",first_number+second_number,"<br>", "Sub of two variables : ",first_number-second_number,"<br>", "Multiplication of two variables : ",first_number*second_number,"<br>", "Division of two variables : ",first_number/second_number,"<br>", "Modulus of two variables : ",first_number%second_number,"<br>","<br>")
+
+var JS_assignment_marks = 10
+document.write("Value of JS assignment Marks is : ",JS_assignment_marks,"<br>")
+document.write("Value of JS assignment Marks after increement is : ",JS_assignment_marks+1,"<br>")
+document.write("Value of JS assignment Marks after adding 7 is : ",JS_assignment_marks+7,"<br>")
+document.write("Value of JS assignment Marks after deecrement is : ",JS_assignment_marks-3,"<br>")
+document.write("Remainder Value of JS assignment Marks  is : ",JS_assignment_marks/3,"<br>","<br>")
+
+var ticket_price = 600
+
+document.write("Total Price of 5 x tickets is : ",ticket_price*5,"<br>","<br>")
+
+var hours = 24
+
+document.write("Total hours in a day: ",hours*1,"<br>")
+document.write("Total hours in a week: ",hours*7,"<br>")
+document.write("Total hours in a month: ",hours*30,"<br>")
+document.write("Total hours in a year: ",hours*365,"<br>","<br>")
+
+var Body_temprature = 36.5
+document.write("Normal Body temprature is : ",Body_temprature,"C and it is eqauls to ",Body_temprature*9/5+32, " F","<br>","<br>")
+
+//var current_year = 2023
+//var User_birth_year = prompt ("Enter Your Year Of Birth")
+//document.write("Recpectable your age is : ",current_year-User_birth_year)
+
+var Jeans_price = 1800
+var Belt_price = 550
+var Delivery_charges = 150
+
+document.write("Price of Jeans is : ",Jeans_price,"<br>")
+document.write("Total Cost of 3 x Jeans is : ",Jeans_price*3,"<br>")
+document.write("Price of Jeans is : ",Belt_price,"<br>")
+document.write("Total Cost of 3 x belt is : ",Belt_price*3,"<br>")
+document.write("DC is : ",Delivery_charges,"<br>")
+document.write("Total Cost: ",Delivery_charges+Belt_price+Jeans_price,"<br>","<br>")
+
+var faviorate_snacks = "Almond"
+var current_age = 31
+var Max_age = 55
+var snacks_per_day = 8
+var bal_age = Max_age-current_age
+document.write(bal_age*snacks_per_day,"<br>","<br>")
+
+var radius_circle = 20
+var circumference 
